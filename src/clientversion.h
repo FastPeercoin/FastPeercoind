b6e9323b8da9b0af79d0aadebf24c80e6d86d7dd
Chapman Shoop <chapman.shoop@riseup.net>
2025-03-14 10:01:45 -0500

    Removing CLIENT_VERSION_IS_RELEASE

--- src/clientversion.h
+++ src/clientversion.h
@@ -14,9 +14,6 @@
 #define CLIENT_VERSION_REVISION    6
 #define CLIENT_VERSION_BUILD       0
 
-// Set to true for release, false for prerelease or test build
-#define CLIENT_VERSION_IS_RELEASE  true
-
 #endif //HAVE_CONFIG_H
 
 // ppcoin version - intended for display purpose ONLY
