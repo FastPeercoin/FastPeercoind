56b54c7d14b32b2715f927994785e93ed6624694
Chapman Shoop <chapman.shoop@riseup.net>
2025-03-19 14:14:29 -0500

    Don't need this test key anymore.

--- src/alert.cpp
+++ src/alert.cpp
@@ -20,7 +20,6 @@ map<uint256, CAlert> mapAlerts;
 CCriticalSection cs_mapAlerts;
 
 static const char* pszMainKey = "04201f0f85178950503c20d5a947883dff81e727533f1f1da104755fa25275cf68c442b8ad50da3152c10a74ea621da614d3d2048ba25a14f39bfc40c41223543a";
-static const char* pszTestKey = "04383862439513e940f6fcbf62d365c162a5256920c2c25b0b4266fdee4a443d71cfe224dbccff6fdb2ea57a37eb0cbec5637ebea06f63c70ca093672fbdc27643";
 
 void CUnsignedAlert::SetNull()
 {
